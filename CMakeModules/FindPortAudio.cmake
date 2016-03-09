756eceeffc629e849d5868b3db0bcedccdb385d9
Lien Chiang <xsoameix@gmail.com>
2016-03-09 22:40:54 +0800

    Remove *_CFAGS_OTHER and *_LIBRARY in cmake files
    Remove these variables in cmake file:
    * xxx_CFLAGS_OTHERS => Use find_package(Threads) and '-pthread' instead
    * xxx_LIBRARY       => Use xxx_LIBRARIES instead

--- CMakeModules/FindPortAudio.cmake
+++ CMakeModules/FindPortAudio.cmake
@@ -3,7 +3,6 @@
 #
 #  PORTAUDIO_FOUND       - system has PortAudio
 #  PORTAUDIO_INCLUDE_DIR - the PortAudio include directory
-#  PORTAUDIO_LIBRARY     - the PortAudio library
 #  PORTAUDIO_LIBRARIES   - the libraries needed to use PortAudio
 #
 #  Copyright (c) 2016 Lien Chiang  <xsoameix@gmail.com>
@@ -15,13 +14,11 @@ if ("${CMAKE_C_COMPILER_ID}" STREQUAL "MSVC")
 else()
   find_package(PkgConfig QUIET REQUIRED)
   pkg_search_module(PORTAUDIO REQUIRED QUIET portaudio-2.0)
-  set(PORTAUDIO_LIBRARIES ${PORTAUDIO_LIBRARIES} ${PORTAUDIO_CFLAGS_OTHER})
   find_path(PORTAUDIO_INCLUDE_DIR NAMES portaudio.h HINTS ${PORTAUDIO_INCLUDE_DIRS})
-  find_library(PORTAUDIO_LIBRARY NAMES portaudio HINTS ${PORTAUDIO_LIBRARY_DIRS})
   find_package(PackageHandleStandardArgs QUIET REQUIRED)
   find_package_handle_standard_args(PortAudio
-    REQUIRED_VARS PORTAUDIO_LIBRARY PORTAUDIO_LIBRARIES PORTAUDIO_INCLUDE_DIR
+    REQUIRED_VARS PORTAUDIO_LIBRARIES PORTAUDIO_INCLUDE_DIR
     VERSION_VAR PORTAUDIO_VERSION)
 endif()
 
-mark_as_advanced(PORTAUDIO_INCLUDE_DIR PORTAUDIO_LIBRARY PORTAUDIO_LIBRARIES)
+mark_as_advanced(PORTAUDIO_INCLUDE_DIR PORTAUDIO_LIBRARIES)
